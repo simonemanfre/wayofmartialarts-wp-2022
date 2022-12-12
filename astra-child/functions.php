515685535047eb5d8a4f87c082099dd0451614ff
simonemanfre <simone.manfredini@trapella.it>
2022-12-12 20:38:44 +0100

    [feat] blocco autore custom

--- astra-child/functions.php
+++ astra-child/functions.php
@@ -7,6 +7,7 @@
 add_action( 'wp_enqueue_scripts', 'astra_child_enqueue_scripts_styles' );
 function astra_child_enqueue_scripts_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css', array(), '1.0.971', 'all' );
+   wp_enqueue_style( 'child-style', get_stylesheet_directory_uri().'/style.css', array(), '1.0.971', 'all' );
    wp_enqueue_style( 'astra-child-theme', get_stylesheet_directory_uri() . '/customizations/assets/css/main.css', array(), '1.0.0.1898', 'all' );
 
    wp_enqueue_script( 'astra-child-theme', get_stylesheet_directory_uri() . '/customizations/assets/js/main.js', array('jquery'), '1.0.78891298', 'all', true  );
@@ -45,10 +46,6 @@ require get_theme_file_path( 'customizations/classes/gutenberg/gutenberg.php' );
 *
 */
 require get_theme_file_path( 'customizations/shortcodes/related_posts.php' );
-
-
-//AGGIUNGO SUPPORTO EXCERPT NELLE PAGINE
-add_post_type_support('page', 'excerpt');
 
 
 //CREO SHORTCODE PER VISUALIZZARE L'ANNO CORRENTE
